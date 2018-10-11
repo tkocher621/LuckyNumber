5932f106218cd2ee8c477917648364f884bf5aca
tkocher621 <42099068+tkocher621@users.noreply.github.com>
2018-10-11 18:43:57 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,47 @@
 package com.company;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static Vector<Random> Names = new Vector<>();
 
+    private static void PromptNum(Random person)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(person.name + ", your random perfect square is " + person.perfectRandom());
+        System.out.println("What would you like to do?\n0 - New number\n1 - Listing of names and numbers\n2 - Enter another name");
+
+        int input = scan.nextInt();
+
+        if (input == 0)
+        {
+            PromptNum(person);
+        }
+        else if (input == 1)
+        {
+            for (Random dude : Names)
+            {
+                System.out.println(dude.name + " - " + dude.num);
+            }
+        }
+        else if (input == 2)
+        {
+            PromptName();
+        }
+    }
+
+    private static void PromptName()
+    {
         Scanner scan = new Scanner(System.in);
         System.out.println("What's your name?");
         Random person = new Random(scan.nextLine());
-        System.out.println("Hello " + person.name + "! Your random perfect square is " + person.perfectRandom());
+        System.out.println("Hello " + person.name + "!");
+        Names.add(person);
+        PromptNum(person);
+    }
 
+    public static void main(String[] args)
+    {
+        PromptName();
     }
 }
